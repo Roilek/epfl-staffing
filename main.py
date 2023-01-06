fe22138499a4f92a5e0cfdec657419d15c019d7a
Roilek <elior.papiernik@gmail.com>
2023-01-06 07:00:40 +0100

    Update structure to be modular

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-# python-telegram-bot that will do a conversation with the user to get informations about a post the bot will make to a channel with specific formatting
-
+import datetime
 import logging
 import os
+from typing import Callable
 
+import emoji
 from dotenv import load_dotenv
+from telegram import ReplyKeyboardRemove, Update, ReplyKeyboardMarkup
 from telegram.constants import ParseMode
-from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Update
 from telegram.ext import CommandHandler, MessageHandler, filters, ConversationHandler, CallbackContext, Application
-
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -15,123 +15,174 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 logger = logging.getLogger(__name__)
 
-# States
+# Variables
 
-POST, TITLE, EMOJI, DATE, DESCRIPTION, LINK, CONTACT, CONFIRMATION = range(8)
+POST_WARNING_MESSAGE = "Please read carefully the rules of posting with /rules before sending your post as any post " \
+                       "that does not comply with these rules will not be posted!"
+
+fields_and_questions = {
+    'title': 'What is the title of the post?',
+    'emoji': 'What is the emoji of the post?',
+    'date': 'What is the date of the post?',
+    'description': 'What is the description of the post?',
+    'link': 'What is the link of the post?',
+    'contact': 'What is the contact of the post?',
+    'confirmation': 'Send \'Yes\' to confirm?',
+}
+
+LIMITS = {
+    'title': 10,
+    'emoji': 1,
+    'description': 100,
+    'link': 100,
+    'contact': 50,
+}
+
+TITLE, EMOJI, DATE, DESCRIPTION, LINK, CONTACT, CONFIRMATION = fields_and_questions.keys()
+flow = [TITLE, EMOJI, DATE, DESCRIPTION, LINK, CONTACT, CONFIRMATION]
+
+CONFIRM = '✅'
+DENY = '❌'
 
 # Global variables
 
 post = {}
 
 
+# Helper functions
+
+async def build_post(update: Update, context: CallbackContext) -> str:
+    """Builds the post to be sent to the channel"""
+    print(post)
+    text = ""
+    for field in flow:
+        if field in post:
+            text += f"{field}: {post[field]}\n"
+    return text
+
+
+def is_shorter_than(length: int) -> Callable[[str], bool]:
+    """Returns a function that checks if a string is shorter than a given length."""
+    return lambda s: len(s) < length
+
+
+def is_emoji() -> Callable[[str], bool]:
+    """Returns a function that checks if a string is an emoji."""
+    return lambda s: emoji.emoji_count(s) == 1 and len(s) == 1
+
+
+def check_one_date(text: str) -> bool:
+    """Checks one date"""
+    if len(text) == 5 and text[2] == '/':
+        try:
+            datetime.datetime.strptime(text, '%d/%m')
+            return True
+        except ValueError:
+            return False
+    return False
+
+
+def is_date() -> Callable[[str], bool]:
+    """Returns a function that checks if a string is a date."""
+    return lambda s: check_one_date(s) if len(s) == 5 else (
+            len(s) == 11 and s[5] == '-' and check_one_date(s[:5]) and check_one_date(
+        s[6:]) and datetime.datetime.strptime(s[:5], '%d/%m') < datetime.datetime.strptime(s[6:], '%d/%m'))
+
+
+async def send_confirmation_menu(update: Update, context: CallbackContext) -> None:
+    """."""
+    reply_keyboard = [['Yes', 'No']]
+    # build the post and send it if the keyboard
+    text = await build_post(update, context)
+    await update.message.reply_text(text, reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
+
+    return
+
+
+async def go_next(update: Update, context: CallbackContext, field: str, rule: Callable[[str], bool],
+                  **kwargs) -> str:
+    """Stores the confirmation and ends the conversation."""
+    user = update.message.from_user
+    text = update.message.text
+    logger.info("%s of %s: %s", field, user.first_name, text)
+    while not rule(text):
+        await update.message.reply_text(fields_and_questions[field])
+        return field
+    post[field] = update.message.text
+
+    # reply with question of next field
+    next_field = flow[flow.index(field) + 1]
+
+    if next_field == CONFIRMATION:
+        await send_confirmation_menu(update, context)
+        await update.message.reply_text(fields_and_questions[next_field], reply_markup=ReplyKeyboardRemove())
+        return next_field
+
+    await update.message.reply_text(fields_and_questions[next_field])
+
+    return next_field
+
+
 # Functions
 
 async def start(update: Update, context: CallbackContext) -> None:
     """Send a message when the command /start is issued."""
-    print(update)
-    await update.message.reply_text('Hi!')
+    text = "Hello!\n"
+    text += "You can use /post to request a new post on the EPFL Staffing channel (https://t.me/+fkbWQxKHD58xNGI8)"
+    await update.message.reply_text(text)
     return
 
 
 async def help_command(update: Update, context: CallbackContext) -> None:
     """Send a message when the command /help is issued."""
-    await update.message.reply_text('Help!')
+    await update.message.reply_text('Help! [Work in progress...]')
     return
 
 
-async def new_post(update: Update, context: CallbackContext) -> int:
+async def new_post(update: Update, context: CallbackContext) -> str:
     """Starts the conversation and asks the user about the post."""
-    await update.message.reply_text(
-        'Hi! I will ask you a few questions about the post you want to create. '
-        'You can use /cancel to stop talking to me.\n\n'
-        'What is the title of the post?')
-    return TITLE
+    await update.message.reply_text(POST_WARNING_MESSAGE)
+    first_field = flow[0]
+    await update.message.reply_text(fields_and_questions[first_field])
+    return first_field
 
 
-async def title(update: Update, context: CallbackContext) -> int:
-    """Stores the title and asks for an emoji."""
-    user = update.message.from_user
-    logger.info("Title of %s: %s", user.first_name, update.message.text)
-    post['title'] = update.message.text
-
-    await update.message.reply_text('What is the emoji of the post?')
-
-    return EMOJI
+async def title(update: Update, context: CallbackContext) -> str:
+    """Stores the title."""
+    return await go_next(update, context, TITLE, is_shorter_than(LIMITS[TITLE]))
 
 
-async def emoji(update: Update, context: CallbackContext) -> None:
-    """Stores the emoji and asks for the date of the event."""
-    user = update.message.from_user
-    logger.info("Emoji of %s: %s", user.first_name, update.message.text)
-    post['emoji'] = update.message.text
-
-    await update.message.reply_text('What is the date of the post?')
-
-    return DATE
+async def myemoji(update: Update, context: CallbackContext) -> str:
+    """Stores the emoji."""
+    return await go_next(update, context, EMOJI, is_emoji())
 
 
-async def date(update: Update, context: CallbackContext) -> int:
-    """Stores the date and asks for a description."""
-    user = update.message.from_user
-    logger.info("Date of %s: %s", user.first_name, update.message.text)
-    post['date'] = update.message.text
-
-    await update.message.reply_text('What is the description of the post?')
-
-    return DESCRIPTION
+async def date(update: Update, context: CallbackContext) -> str:
+    """Stores the date."""
+    return await go_next(update, context, DATE, is_date())
 
 
-async def description(update: Update, context: CallbackContext) -> int:
-    """Stores the description and asks for a link."""
-    user = update.message.from_user
-    logger.info("Description of %s: %s", user.first_name, update.message.text)
-    post['description'] = update.message.text
-
-    await update.message.reply_text('What is the link of the post?')
-
-    return LINK
+async def description(update: Update, context: CallbackContext) -> str:
+    """Stores the description."""
+    return await go_next(update, context, DESCRIPTION, is_shorter_than(LIMITS[DESCRIPTION]))
 
 
-async def link(update: Update, context: CallbackContext) -> int:
-    """Stores the link and asks for a contact."""
-    user = update.message.from_user
-    logger.info("Link of %s: %s", user.first_name, update.message.text)
-    post['link'] = update.message.text
-
-    await update.message.reply_text('What is the contact of the post?')
-
-    return CONTACT
+async def link(update: Update, context: CallbackContext) -> str:
+    """Stores the link."""
+    return await go_next(update, context, LINK, is_shorter_than(LIMITS[LINK]))
 
 
-async def contact(update: Update, context: CallbackContext) -> int:
+async def contact(update: Update, context: CallbackContext) -> str:
     """Stores the contact and asks for confirmation."""
-    user = update.message.from_user
-    logger.info("COntact of %s: %s", user.first_name, update.message.text)
-    post['contact'] = update.message.text
-
-    reply_keyboard = [['Yes', 'No']]
-
-    await update.message.reply_text(
-        'Please confirm the post:\n\n'
-        f'Title: {post["title"]}\n'
-        f'Emoji: {post["emoji"]}\n'
-        f'Date: {post["date"]}\n'
-        f'Description: {post["description"]}\n'
-        f'Link: {post["link"]}\n\n'
-        f'Contact: {post["contact"]}\n\n'
-        'Is this correct?',
-        reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
-
-    return CONFIRMATION
+    return await go_next(update, context, CONTACT, is_shorter_than(LIMITS[CONTACT]))
 
 
 async def confirmation(update: Update, context: CallbackContext) -> int:
     """Stores the confirmation and ends the conversation."""
     user = update.message.from_user
     logger.info("Confirmation of %s: %s", user.first_name, update.message.text)
-    post['confirmation'] = update.message.text
 
-    if post['confirmation'] == 'Yes':
+    if update.message.text == 'Yes':
         await update.message.reply_text(
             'thanks for the information! I will send the post to the channel now.',
             reply_markup=ReplyKeyboardRemove())
@@ -173,7 +224,7 @@ def main() -> None:
         entry_points=[CommandHandler('post', new_post)],
         states={
             TITLE: [MessageHandler(filters.TEXT & ~filters.COMMAND, title)],
-            EMOJI: [MessageHandler(filters.TEXT & ~filters.COMMAND, emoji)],
+            EMOJI: [MessageHandler(filters.TEXT & ~filters.COMMAND, myemoji)],
             DATE: [MessageHandler(filters.TEXT & ~filters.COMMAND, date)],
             DESCRIPTION: [MessageHandler(filters.TEXT & ~filters.COMMAND, description)],
             LINK: [MessageHandler(filters.TEXT & ~filters.COMMAND, link)],
