57eae400dcb85c3c740aad0146e8948cc89eeb8f
Roilek <elior.papiernik@gmail.com>
2023-01-06 04:49:54 +0100

    Adapt structure to ask the expected fields

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ logger = logging.getLogger(__name__)
 
 # States
 
-POST, TITLE, DESCRIPTION, IMAGE, LINK, CONFIRMATION = range(6)
+POST, TITLE, EMOJI, DATE, DESCRIPTION, LINK, CONTACT, CONFIRMATION = range(8)
 
 # Global variables
 
@@ -48,10 +48,32 @@ async def new_post(update: Update, context: CallbackContext) -> int:
 
 
 async def title(update: Update, context: CallbackContext) -> int:
-    """Stores the title and asks for a description."""
+    """Stores the title and asks for an emoji."""
     user = update.message.from_user
     logger.info("Title of %s: %s", user.first_name, update.message.text)
     post['title'] = update.message.text
+
+    await update.message.reply_text('What is the emoji of the post?')
+
+    return EMOJI
+
+
+async def emoji(update: Update, context: CallbackContext) -> None:
+    """Stores the emoji and asks for the date of the event."""
+    user = update.message.from_user
+    logger.info("Emoji of %s: %s", user.first_name, update.message.text)
+    post['emoji'] = update.message.text
+
+    await update.message.reply_text('What is the date of the post?')
+
+    return DATE
+
+
+async def date(update: Update, context: CallbackContext) -> int:
+    """Stores the date and asks for a description."""
+    user = update.message.from_user
+    logger.info("Date of %s: %s", user.first_name, update.message.text)
+    post['date'] = update.message.text
 
     await update.message.reply_text('What is the description of the post?')
 
@@ -59,21 +81,10 @@ async def title(update: Update, context: CallbackContext) -> int:
 
 
 async def description(update: Update, context: CallbackContext) -> int:
-    """Stores the description and asks for an image."""
+    """Stores the description and asks for a link."""
     user = update.message.from_user
     logger.info("Description of %s: %s", user.first_name, update.message.text)
     post['description'] = update.message.text
-
-    await update.message.reply_text('What is the image of the post?')
-
-    return IMAGE
-
-
-async def image(update: Update, context: CallbackContext) -> int:
-    """Stores the image and asks for a link."""
-    user = update.message.from_user
-    logger.info("Image of %s: %s", user.first_name, update.message.text)
-    post['image'] = update.message.text
 
     await update.message.reply_text('What is the link of the post?')
 
@@ -81,19 +92,32 @@ async def image(update: Update, context: CallbackContext) -> int:
 
 
 async def link(update: Update, context: CallbackContext) -> int:
-    """Stores the link and asks for confirmation."""
+    """Stores the link and asks for a contact."""
     user = update.message.from_user
     logger.info("Link of %s: %s", user.first_name, update.message.text)
     post['link'] = update.message.text
+
+    await update.message.reply_text('What is the contact of the post?')
+
+    return CONTACT
+
+
+async def contact(update: Update, context: CallbackContext) -> int:
+    """Stores the contact and asks for confirmation."""
+    user = update.message.from_user
+    logger.info("COntact of %s: %s", user.first_name, update.message.text)
+    post['contact'] = update.message.text
 
     reply_keyboard = [['Yes', 'No']]
 
     await update.message.reply_text(
         'Please confirm the post:\n\n'
         f'Title: {post["title"]}\n'
+        f'Emoji: {post["emoji"]}\n'
+        f'Date: {post["date"]}\n'
         f'Description: {post["description"]}\n'
-        f'Image: {post["image"]}\n'
         f'Link: {post["link"]}\n\n'
+        f'Contact: {post["contact"]}\n\n'
         'Is this correct?',
         reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
 
@@ -112,7 +136,10 @@ async def confirmation(update: Update, context: CallbackContext) -> int:
             reply_markup=ReplyKeyboardRemove())
 
         # Send the post to the channel
-        await context.bot.send_message(chat_id=os.getenv('CHANNEL_ID'), text=f'<b>{post["title"]}</b>\n\n{post["description"]}\n\n<a href="{post["link"]}">Read more</a>', parse_mode=ParseMode.HTML)
+        await context.bot.send_message(
+            chat_id=os.getenv('CHANNEL_ID'),
+            text=f'<b>{post["title"]}</b>\n\n{post["description"]}\n\n<a href="{post["link"]}">Read more</a>',
+            parse_mode=ParseMode.HTML)
         return ConversationHandler.END
     else:
         await update.message.reply_text(
@@ -145,9 +172,11 @@ def main() -> None:
         entry_points=[CommandHandler('post', new_post)],
         states={
             TITLE: [MessageHandler(filters.TEXT & ~filters.COMMAND, title)],
+            EMOJI: [MessageHandler(filters.TEXT & ~filters.COMMAND, emoji)],
+            DATE: [MessageHandler(filters.TEXT & ~filters.COMMAND, date)],
             DESCRIPTION: [MessageHandler(filters.TEXT & ~filters.COMMAND, description)],
-            IMAGE: [MessageHandler(filters.TEXT & ~filters.COMMAND, image)],
             LINK: [MessageHandler(filters.TEXT & ~filters.COMMAND, link)],
+            CONTACT: [MessageHandler(filters.TEXT & ~filters.COMMAND, contact)],
             CONFIRMATION: [MessageHandler(filters.TEXT & ~filters.COMMAND, confirmation)],
         },
         fallbacks=[CommandHandler('cancel', cancel)],
